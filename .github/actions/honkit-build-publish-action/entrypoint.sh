959d196ee46c5e8151119896d2b560bb8fdcec59
Fudgedotdotdot <28399056+Fudgedotdotdot@users.noreply.github.com>
2024-07-19 16:14:06 +0200

    attempting to push to repo from ci

--- .github/actions/honkit-build-publish-action/entrypoint.sh
+++ .github/actions/honkit-build-publish-action/entrypoint.sh
@@ -1,27 +1,15 @@
-# Inspired by https://github.com/onejar99/gitbook-build-publish-action
-
-# function checkIfErr() {
-#     ret=$?
-#     echo "ret=[${ret}]"
-#     if [ ! $ret = '0' ]; then
-#         echo "Oops something wrong! exit code: ${ret}"
-#         exit $ret;
-#     fi
-# }
-
 echo '[INFO] Building static website...'
 cd ${GITHUB_WORKSPACE}
 git config --global --add safe.directory /github/workspace
 
 honkit build
 
-
 echo '[INFO] Some magic to merge main into gh-pages...'
 git fetch origin gh-pages --depth 1
 git checkout gh-pages
 git rebase main
 
-cp -R _book/* .s
+cp -R _book/* .
 git clean -fx node_modules
 git clean -fx _book
 git add .
@@ -29,6 +17,7 @@ git add .
 git config --local user.name "Fudgedotdotdot"
 git config --local user.email "28399056+Fudgedotdotdot@users.noreply.github.com"
 
-git commit -am "Update docs"
+echo '[INFO] Pushing gh-pages...'
+git commit -am "Updated with:  $(echo $GITHUB_SHA | cut -c1-7))"
 git push origin HEAD:gh-pages --force
 git checkout main
