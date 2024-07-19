84944063eb1f5d24a4167b39648c1f94f8201da8
Fudgedotdotdot <28399056+Fudgedotdotdot@users.noreply.github.com>
2024-07-19 16:04:18 +0200

    attempting to push to repo from ci

--- .github/actions/honkit-build-publish-action/entrypoint.sh
+++ .github/actions/honkit-build-publish-action/entrypoint.sh
@@ -14,19 +14,14 @@ cd ${GITHUB_WORKSPACE}
 git config --global --add safe.directory /github/workspace
 
 honkit build
-ls -alt _book/
 
-git branch
-git remote -v
-echo $(git config -l)
 
+echo '[INFO] Some magic to merge main into gh-pages...'
 git fetch origin gh-pages --depth 1
 git checkout gh-pages
 git rebase main
 
-# now gh-pages and main are the same, prepare the correct directory structure for github pages
-cp -R _book/* .
-
+cp -R _book/* .s
 git clean -fx node_modules
 git clean -fx _book
 git add .
@@ -35,36 +30,5 @@ git config --local user.name "Fudgedotdotdot"
 git config --local user.email "28399056+Fudgedotdotdot@users.noreply.github.com"
 
 git commit -am "Update docs"
-git reflog show
-
-git push -u origin gh-pages
-
+git push origin HEAD:gh-pages --force
 git checkout main
-
-# # checkout to the gh-pages branch
-# git checkout gh-pages
-
-# # pull the latest updates
-# git pull origin gh-pages --rebase
-
-# # copy the static site files into the current directory.
-# cp -R _book/* .
-
-# # remove 'node_modules' and '_book' directory
-# git clean -fx node_modules
-# git clean -fx _book
-
-# # add all files
-# git add .
-
-# git log
-
-
-# commit
-#git commit -a -m "Update docs"
-
-# push to the origin
-#git push origin gh-pages
-
-# checkout to the master branch
-#git checkout master
